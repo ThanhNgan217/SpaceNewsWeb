274123fea48f2bad7eeedab0c5795fddea4c67b5
ThanhNgan217 <129735971+ThanhNgan217@users.noreply.github.com>
2023-04-17 17:38:47 +0700

    commit part2

--- SpaceNews/src/app/app-routing.module.ts
+++ SpaceNews/src/app/app-routing.module.ts
@@ -4,13 +4,21 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { ListPostComponent } from './list-post/list-post.component';
 import { PostComponent } from './post/post.component';
+import { ContentComponent } from './content/content.component';
 
 const routes: Routes = [
-  {path:'', component:MainPageComponent},
+  {
+    path:'',
+    component:MainPageComponent,
+    children:[
+      {path:'', component:ContentComponent},
+      {path:'post/:id', component:PostComponent}
+    ]
+  },
   {path:'home',redirectTo:'', pathMatch:'full'},
+  {path:'post/:id', component:PostComponent},
   {path:'login', component:LoginPageComponent},
-  // {path:'home/topic/:id', component:ListPostComponent},
-  {path:'post/:id', component:PostComponent}
+  // {path:'/', component:ListPostComponent},
 ];
 
 @NgModule({
--- SpaceNews/src/app/post/post.component.ts
+++ SpaceNews/src/app/post/post.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from '../post';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +9,100 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-
-  constructor() { }
-
+  ListPosts : Post[] = [
+    {
+      id : 1,
+      idTopic : 1,
+      title : "Rijeka begins year of European Capital of ...",
+      url : '/assets/img/event-img/event1.png',
+      // new Date ( year, month(octal : hệ bát phân), date[, hour, minute, second, millisecond ])
+      time : new Date(2023, 0o3, 0o5, 9, 30, 0, 0),
+      author : 'EuroNews',
+      counter : '4 min read'
+    },
+    {
+      id : 2,
+      idTopic : 2,
+      title : "Rijeka begins year of European Capital of ...",
+      url : '/assets/img/event-img/event2.png',
+      time : new Date(2023, 0o4, 0o5, 9, 30, 0, 0),
+      author : 'EuroNews',
+      counter : '4 min read'
+    },
+    {
+      id : 3,
+      title : "Rijeka begins year of European Capital of ...",
+      idTopic : 3,
+      url : '/assets/img/event-img/event3.png',
+      time : new Date(2023, 0o5, 0o5, 9, 30, 0, 0), //6
+      author : 'EuroNews',
+      counter : '4 min read'
+    },
+    {
+      id : 4,
+      title : "Rijeka begins year of European Capital of ...",
+      idTopic : 1,
+      url : '/assets/img/event-img/event1.png',
+      time : new Date(2023, 0o6, 0o5, 9, 30, 0, 0), // 7
+      author : 'EuroNews',
+      counter : '4 min read'
+    },
+    {
+      id : 5,
+      title : "Rijeka begins year of European Capital of ...",
+      idTopic : 3,
+      url : '/assets/img/event-img/event3.png',
+      time : new Date(2023, 0o7, 0o5, 9, 30, 0, 0), // 8
+      author : 'EuroNews',
+      counter : '4 min read'
+    },
+    {
+      id : 6,
+      title : "Rijeka begins year of European Capital of ...",
+      idTopic : 3,
+      url : '/assets/img/event-img/event3.png',
+      time : new Date(2023, 0o10, 0o5, 9, 30, 0, 0), //9
+      author : 'EuroNews',
+      counter : '4 min read'
+    },
+    {
+      id : 7,
+      title : "Rijeka begins year of European Capital of ...",
+      idTopic : 2,
+      url : '/assets/img/event-img/event2.png',
+      time : new Date(2023, 0o11, 0o5, 9, 30, 0), // 10
+      author : 'EuroNews',
+      counter : '4 min read'
+    },
+    {
+      id : 8,
+      title : "Rijeka begins year of European Capital of ...",
+      idTopic : 3,
+      url : '/assets/img/event-img/event3.png',
+      time : new Date(2023, 0o12, 0o5, 9, 30, 0, 0),//11
+      author : 'EuroNews',
+      counter : '4 min read'
+    },
+    {
+      id : 9,
+      title : "Rijeka begins year of European Capital of ...",
+      idTopic : 2,
+      url : '/assets/img/event-img/event2.png',
+      time : new Date(2023, 0o13, 0o5, 9, 30, 0, 0),//12
+      author : 'EuroNews',
+      counter : '4 min read'
+    },
+  ]
+  PostShow :Post | undefined;
+  constructor(private _route: ActivatedRoute) {}
   ngOnInit(): void {
+    let PostId = this._route.snapshot.paramMap.get('id');
+    this.ListPosts.forEach(p => {
+      if(p.id == Number(PostId)){
+        this.PostShow = p;
+        console.log('success')
+      }
+    });
   }
 
 }
