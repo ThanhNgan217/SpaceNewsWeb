d6046c4730f0b07b5b535ea4816eb8bc4a85fb5b
ThanhNgan217 <129735971+ThanhNgan217@users.noreply.github.com>
2023-04-27 17:18:30 +0700

    commit 27/4

--- SpaceNews/src/app/Service/api.service.ts
+++ SpaceNews/src/app/Service/api.service.ts
@@ -20,17 +20,17 @@ export class ApiService {
 
   logged(id:string, auth:string) {
     this.Isloged.next([id, auth]);
-
-    // alert('doit')
   }
 
   private httpOptions = {
-    // headers : new HttpHeaders({'accept':'*/*', 'Content-Type': 'application/json'})
-    // headers : new HttpHeaders({'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJuZ2FuQGdtYWlsLmNvbSIsImp0aSI6IjdkOWY4ZTA3LTBlNjgtNGI4Yi1hY2MxLWNhNDcxMDMzMWVlYyIsImlhdCI6MTY4MTExMTg2OSwicm9sIjoiYXBpX2FjY2VzcyIsImlkIjoiZjZiOWExOGMtZGM3NC00OWVjLTk5MmItNTNmYTdhYjYzNzEwIiwibmJmIjoxNjgxMTExODY4LCJleHAiOjE2ODExMTkwNjgsImlzcyI6IndlYkFwaSIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6NDQ0OTMvIn0.LV2dR5DUE7UyBZHOEHO7fuvI4MbijK3vyjQDvLNKmp4'})
-    headers : new HttpHeaders({ 'Content-Type': 'application/json;charset=UTF-8',
-    "Access-Control-Allow-Origin": "*",
-    'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
-    'Access-Control-Allow-Headers': '*'}),
+  //   // headers : new HttpHeaders({'accept':'*/*', 'Content-Type': 'application/json'})
+  //   // headers : new HttpHeaders({'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJuZ2FuQGdtYWlsLmNvbSIsImp0aSI6IjdkOWY4ZTA3LTBlNjgtNGI4Yi1hY2MxLWNhNDcxMDMzMWVlYyIsImlhdCI6MTY4MTExMTg2OSwicm9sIjoiYXBpX2FjY2VzcyIsImlkIjoiZjZiOWExOGMtZGM3NC00OWVjLTk5MmItNTNmYTdhYjYzNzEwIiwibmJmIjoxNjgxMTExODY4LCJleHAiOjE2ODExMTkwNjgsImlzcyI6IndlYkFwaSIsImF1ZCI6Imh0dHA6Ly9sb2NhbGhvc3Q6NDQ0OTMvIn0.LV2dR5DUE7UyBZHOEHO7fuvI4MbijK3vyjQDvLNKmp4'})
+  headers : new HttpHeaders({ 'Content-Type': 'application/json;charset=UTF-8',
+  // "Access-Control-Allow-Origin": "*",
+  // "Access-Control-Allow-Headers": "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
+  // 'Access-Control-Allow-Methods':'GET,POST,PUT,PATCH,DELETE',
+  // 'Access-Control-Allow-Headers': '*'
+  }),
   };
 
   constructor(private http:HttpClient) { }
